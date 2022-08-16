801ec5ed73bbd255016147c2ce4e416a1fd38107
ryan <ryanjgecelter@gmail.com>
2022-08-16 15:22:58 +0300

    created new components VendingMachine, Candy, Chips, Drink

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Navbar from './Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Route exact path="/"><VendingMachine/></Route>
+      <Route exact path="/chips"><Chips/></Route>
+      <Route exact path="/drink"><Drink/></Route>
+      <Route exact path="/candy"><Candy/></Route>
     </div>
   );
 }
